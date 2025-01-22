c59bca93310f1bed1ac13dad1327bd14a7d63c9f
Maharshi Desai <133489200+Maharshi-24@users.noreply.github.com>
2025-01-23 02:42:04 +0530

    Updated on tap animations.

--- lib/widgets/chat/animated_message.dart
+++ lib/widgets/chat/animated_message.dart
@@ -30,16 +30,24 @@ class AnimatedMessage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Align(
           alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
-          child: isUser
-              ? Container(
-            padding: const EdgeInsets.fromLTRB(12, 16, 12, 0),
-            decoration: BoxDecoration(
-              color: Colors.grey[900],
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: MessageParser.parse(message, context),
-          )
-              : MessageParser.parse(message, context),
+          child: InkWell(
+            onTap: () {
+              // Handle the tap event here
+              print("Message tapped: $message");
+            },
+            borderRadius: BorderRadius.circular(16), // Ensures the ripple matches the container shape
+            splashColor: Colors.grey.withOpacity(0.3), // Custom splash color
+            child: isUser
+                ? Container(
+                  padding: const EdgeInsets.fromLTRB(12, 16, 12, 0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: MessageParser.parse(message, context),
+                )
+                : MessageParser.parse(message, context),
+          ),
         ),
       ),
     );
