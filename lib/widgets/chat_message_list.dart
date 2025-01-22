522d5487e5871bfe0dd25713a9166b25372bf9a7
Maharshi Desai <133489200+Maharshi-24@users.noreply.github.com>
2025-01-22 18:23:40 +0530

    Added the scroll-to-bottom button.

--- lib/widgets/chat_message_list.dart
+++ lib/widgets/chat_message_list.dart
@@ -4,41 +4,129 @@ import '../providers/chat_provider.dart';
 import 'animated_message.dart';
 import 'thinking_indicator.dart';
 
-class ChatMessageList extends StatelessWidget {
-  final ScrollController _scrollController = ScrollController();
+class ChatMessageList extends StatefulWidget {
+  const ChatMessageList({super.key});
 
-  ChatMessageList({super.key});
+  @override
+  _ChatMessageListState createState() => _ChatMessageListState();
+}
+
+class _ChatMessageListState extends State<ChatMessageList> {
+  final ScrollController _scrollController = ScrollController();
+  bool _showScrollToBottomButton = false;
+  int _previousMessageCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    // Show the scroll-to-bottom button if the user scrolls up
+    if (_scrollController.offset < _scrollController.position.maxScrollExtent - 100) {
+      if (!_showScrollToBottomButton) {
+        setState(() {
+          _showScrollToBottomButton = true;
+        });
+      }
+    } else {
+      if (_showScrollToBottomButton) {
+        setState(() {
+          _showScrollToBottomButton = false;
+        });
+      }
+    }
+  }
+
+  void _scrollToBottom() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<ChatProvider>(
       builder: (context, chatProvider, child) {
-        WidgetsBinding.instance.addPostFrameCallback((_) {
-          if (_scrollController.hasClients) {
-            _scrollController.animateTo(
-              _scrollController.position.maxScrollExtent,
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeOut,
-            );
-          }
-        });
-
-        return ListView.builder(
-          controller: _scrollController,
-          padding: const EdgeInsets.symmetric(vertical: 16),
-          itemCount: chatProvider.messages.length + (chatProvider.isThinking ? 1 : 0),
-          itemBuilder: (context, index) {
-            if (index < chatProvider.messages.length) {
-              final message = chatProvider.messages[index];
-              return AnimatedMessage(
-                key: ValueKey(message['text']),
-                message: message['text'],
-                isUser: message['isUser'],
+        // Scroll to bottom only when a new message is added
+        if (chatProvider.messages.length > _previousMessageCount) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            if (_scrollController.hasClients) {
+              _scrollController.animateTo(
+                _scrollController.position.maxScrollExtent,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeOut,
               );
-            } else {
-              return const ThinkingIndicator();
             }
-          },
+          });
+          _previousMessageCount = chatProvider.messages.length;
+        }
+
+        return Stack(
+          children: [
+            ListView.builder(
+              controller: _scrollController,
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              itemCount: chatProvider.messages.length + (chatProvider.isThinking ? 1 : 0),
+              itemBuilder: (context, index) {
+                if (index < chatProvider.messages.length) {
+                  final message = chatProvider.messages[index];
+                  return AnimatedMessage(
+                    key: ValueKey(message['text']),
+                    message: message['text'],
+                    isUser: message['isUser'],
+                  );
+                } else {
+                  return const ThinkingIndicator();
+                }
+              },
+            ),
+            // Scroll-to-bottom button with fade animation
+            Positioned(
+              bottom: 16,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: AnimatedOpacity(
+                  opacity: _showScrollToBottomButton ? 1.0 : 0.0,
+                  duration: const Duration(milliseconds: 300),
+                  child: GestureDetector(
+                    onTap: _scrollToBottom,
+                    child: Container(
+                      width: 50, // Fixed size for the button
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[700], // Button color
+                        shape: BoxShape.circle, // Make it circular
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            blurRadius: 6,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.arrow_downward,
+                        color: Colors.white,
+                        size: 24, // Larger icon size
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         );
       },
     );
