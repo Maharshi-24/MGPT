5d4cdb4f30c9156bc5b30b170294aaf89f479753
Maharshi Desai <133489200+Maharshi-24@users.noreply.github.com>
2025-01-27 12:10:41 +0530

    Changed the name.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Maharshi Chat',
+      title: 'MGPT',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
