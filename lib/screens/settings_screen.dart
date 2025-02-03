b1f8d23c1acf1a816c298338573ffdf7c5054850
Maharshi Desai <133489200+Maharshi-24@users.noreply.github.com>
2025-02-03 11:24:45 +0530

    Fixed the logout in the settings screen.

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart'; // Import for HapticFeedback
 import 'package:firebase_auth/firebase_auth.dart';
 import 'changelog_screen.dart';
 import '../services/notification_service.dart';
+import 'login_screen.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -38,11 +39,19 @@ class _SettingsScreenState extends State<SettingsScreen> {
     }
   }
 
-  Widget _buildListTile(IconData icon, String title, {String? subtitle, Color? iconColor, VoidCallback? onTap}) {
+  Widget _buildListTile(
+      IconData icon,
+      String title, {
+        String? subtitle,
+        Color? iconColor,
+        VoidCallback? onTap,
+      }) {
     return ListTile(
       leading: Icon(icon, color: iconColor ?? Colors.white),
       title: Text(title, style: TextStyle(color: Colors.white)),
-      subtitle: subtitle != null ? Text(subtitle, style: TextStyle(color: Colors.grey[400])) : null,
+      subtitle: subtitle != null
+          ? Text(subtitle, style: TextStyle(color: Colors.grey[400]))
+          : null,
       onTap: () {
         // Add haptic feedback
         HapticFeedback.lightImpact();
@@ -85,28 +94,34 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 SizedBox(width: 16),
                 Text(
                   _userName,
-                  style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ],
             ),
           ),
           _buildListTile(Icons.email_outlined, 'Email', subtitle: _userEmail),
-          _buildListTile(Icons.phone_outlined, 'Phone number', subtitle: _userPhone),
+          _buildListTile(Icons.phone_outlined, 'Phone number',
+              subtitle: _userPhone),
           _buildListTile(Icons.palette_outlined, 'Customize'),
           _buildListTile(Icons.security, 'Data Controls'),
           _buildListTile(Icons.mic, 'Voice'),
           _buildListTile(Icons.info_outline, 'About'),
           _buildListTile(Icons.new_releases, "What's New", onTap: () {
             Navigator.of(context).push(PageRouteBuilder(
-              pageBuilder: (context, animation, secondaryAnimation) => ChangelogScreen(onDone: () {}),
-              transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  ChangelogScreen(onDone: () {}),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
                 const begin = Offset(1.0, 0.0);
                 const end = Offset.zero;
                 const curve = Curves.easeInOut;
-
-                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                var tween =
+                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
                 var offsetAnimation = animation.drive(tween);
-
                 return SlideTransition(position: offsetAnimation, child: child);
               },
             ));
@@ -118,7 +133,12 @@ class _SettingsScreenState extends State<SettingsScreen> {
           }),
           _buildListTile(Icons.logout, 'Sign out', iconColor: Colors.red, onTap: () async {
             await _auth.signOut();
-            Navigator.of(context).pushReplacementNamed('/login');
+            // Navigate to the login page and remove all previous routes from the stack
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginScreen()), // Replace with your login screen widget
+                  (Route<dynamic> route) => false, // This ensures that all previous routes are removed
+            );
           }),
         ],
       ),
