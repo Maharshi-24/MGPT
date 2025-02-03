94a1c591fe76728acddda8f85a2b7d1bcba8f812
Maharshi Desai <133489200+Maharshi-24@users.noreply.github.com>
2025-02-03 15:25:32 +0530

    Optimized the chat screen.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,7 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../widgets/chat/chat_message_list.dart';
 import '../widgets/chat/chat_input.dart';
+import '../widgets/common/scroll_to_bottom_button.dart';
 import '../widgets/drawer/custom_drawer.dart';
+import '../widgets/chat/chat_app_bar.dart';
 import '../providers/chat_provider.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -17,7 +19,6 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
   bool _isDrawerOpen = false;
   double _drawerOffset = 0.0;
   final ScrollController _scrollController = ScrollController();
-  final GlobalKey _tripleDotKey = GlobalKey();
 
   @override
   void initState() {
@@ -58,23 +59,14 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
     }
   }
 
-  void _clearMessages() {
-    final chatProvider = Provider.of<ChatProvider>(context, listen: false);
-    chatProvider.clearMessages();
-  }
-
   @override
   Widget build(BuildContext context) {
     final chatProvider = Provider.of<ChatProvider>(context);
-
-    // Check if the last message is "What can I help with?" or if there are no messages
     bool showScrollDownIcon = chatProvider.messages.isNotEmpty &&
         chatProvider.messages.last['text'] != 'What can I help with?';
 
     return GestureDetector(
-      onTap: () {
-        chatProvider.focusNode.unfocus();
-      },
+      onTap: () => chatProvider.focusNode.unfocus(),
       onHorizontalDragUpdate: (details) {
         setState(() {
           _drawerOffset = (_drawerOffset + details.primaryDelta! / MediaQuery.of(context).size.width)
@@ -101,65 +93,18 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
               ),
               child: Column(
                 children: [
-                  AppBar(
-                    title: const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'MGPT',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    backgroundColor: Colors.black,
-                    elevation: 0,
-                    leading: IconButton(
-                      icon: const Icon(
-                        Icons.short_text,
-                        color: Colors.white,
-                        size: 38,
-                      ),
-                      onPressed: _toggleDrawer,
-                    ),
-                    actions: [
-                      IconButton(
-                        icon: const Icon(
-                          Icons.drive_file_rename_outline,
-                          color: Colors.white,
-                        ),
-                        onPressed: _clearMessages,
-                      ),
-                      InkWell(
-                        key: _tripleDotKey,
-                        borderRadius: BorderRadius.circular(20),
-                        onTap: () {
-                          HapticFeedback.lightImpact();
-                          _showCustomPopupMenu(context);
-                        },
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Icon(
-                            Icons.more_vert,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  ChatAppBar(onDrawerToggle: _toggleDrawer),
                   Expanded(
-                    child: ChatMessageList(
-                      scrollController: _scrollController,
-                    ),
+                    child: ChatMessageList(scrollController: _scrollController),
                   ),
                   const ChatInput(),
                 ],
               ),
             ),
-            // Conditionally show the background overlay if drawer is open
             if (_isDrawerOpen || _drawerOffset > 0)
               GestureDetector(
                 onTap: _toggleDrawer,
-                child: Container(
-                  color: Colors.black.withOpacity(0.5 * _drawerOffset),
-                ),
+                child: Container(color: Colors.black.withOpacity(0.5 * _drawerOffset)),
               ),
             AnimatedContainer(
               duration: const Duration(milliseconds: 300),
@@ -171,96 +116,15 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
               ),
               child: const CustomDrawer(),
             ),
-            // Conditionally show scroll down icon
             if (showScrollDownIcon)
               Positioned(
-                bottom: 10,
-                right: 10,
-                child: GestureDetector(
-                  onTap: _scrollToBottom,
-                  child: Icon(
-                    Icons.arrow_downward,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                ),
+                bottom: 150,
+                right: 480,
+                child: ScrollToBottomButton(onPressed: _scrollToBottom),
               ),
           ],
         ),
       ),
-    );
-  }
-
-  void _showCustomPopupMenu(BuildContext context) {
-    final RenderBox renderBox = _tripleDotKey.currentContext!.findRenderObject() as RenderBox;
-    final Offset offset = renderBox.localToGlobal(Offset.zero);
-
-    showMenu<String>(
-      context: context,
-      position: RelativeRect.fromLTRB(
-        offset.dx,
-        offset.dy + renderBox.size.height,
-        offset.dx + renderBox.size.width,
-        offset.dy + renderBox.size.height + 100,
-      ),
-      items: <PopupMenuEntry<String>>[
-        const PopupMenuItem<String>(
-          value: 'view_details',
-          child: ListTile(
-            leading: Icon(Icons.info_outline, color: Colors.white),
-            title: Text('View Details', style: TextStyle(color: Colors.white)),
-          ),
-        ),
-        const PopupMenuItem<String>(
-          value: 'share',
-          child: ListTile(
-            leading: Icon(Icons.share_outlined, color: Colors.white),
-            title: Text('Share', style: TextStyle(color: Colors.white)),
-          ),
-        ),
-        const PopupMenuDivider(),
-        const PopupMenuItem<String>(
-          value: 'rename',
-          child: ListTile(
-            leading: Icon(Icons.edit_outlined, color: Colors.white),
-            title: Text('Rename', style: TextStyle(color: Colors.white)),
-          ),
-        ),
-        const PopupMenuItem<String>(
-          value: 'archive',
-          child: ListTile(
-            leading: Icon(Icons.archive_outlined, color: Colors.white),
-            title: Text('Archive', style: TextStyle(color: Colors.white)),
-          ),
-        ),
-        const PopupMenuItem<String>(
-          value: 'delete',
-          child: ListTile(
-            leading: Icon(Icons.delete_outline, color: Colors.white),
-            title: Text('Delete', style: TextStyle(color: Colors.white)),
-          ),
-        ),
-        const PopupMenuItem<String>(
-          value: 'move_to_project',
-          child: ListTile(
-            leading: Icon(Icons.folder_open_outlined, color: Colors.white),
-            title: Text('Move to Project', style: TextStyle(color: Colors.white)),
-          ),
-        ),
-        const PopupMenuDivider(),
-        const PopupMenuItem<String>(
-          value: 'temporary_chat',
-          child: ListTile(
-            leading: Icon(Icons.chat_bubble_outline, color: Colors.white),
-            title: Text('Temporary Chat', style: TextStyle(color: Colors.white)),
-          ),
-        ),
-      ],
-      elevation: 8,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      color: const Color(0xFF141414),
     );
   }
 }
